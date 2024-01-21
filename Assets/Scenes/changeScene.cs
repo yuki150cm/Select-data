a88703534da6bfdb43362337b112097310d22f84
Yuki Ouchi s1280044 <86813840+yuki150cm@users.noreply.github.com>
2024-01-21 18:55:55 +0900

    Add files via upload

---
+++ Assets/Scenes/changeScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public void OK_Button()
+    {
+        Select_JSON_data data = new Select_JSON_data();
+        data.path = Select_data.path;
+        data.sid = Select_data.sid;
+        data.condition = Select_data.condition;
+        string json = JsonUtility.ToJson(data);
+
+        System.IO.File.WriteAllText("/Users/ohuchiyuki/UoA/4year/Plot_data_var2/Assets/Resources/JSON/Select_data.json",json);
+    }
+}
